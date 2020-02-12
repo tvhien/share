2f53b9459db321bf9351f961d60a9ca29f8ebde7
tvhien <59992495+tvhien@users.noreply.github.com>
2020-02-12 14:18:48 +0700

    service

---
+++ service.pp
@@ -0,0 +1,14 @@
+# Manage the state of the nginx daemon
+#
+# @summary Manage the state of the nginx daemon
+#
+# @example
+#   include nginx::service
+class nginx::service {
+  service { 'nginx_service':
+    name       => 'nginx',
+    ensure     => 'running',
+    enable     => true,
+    hasrestart => true,
+  }
+}
